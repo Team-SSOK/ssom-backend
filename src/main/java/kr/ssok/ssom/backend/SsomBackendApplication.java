0bd12f4407cd05148f1fed722ce3721fd6090051
구동혁 <dhku@naver.com>
2025-05-26 10:07:09 +0900

    feat: 프로젝트 및 의존성 설정

---
+++ src/main/java/kr/ssok/ssom/backend/SsomBackendApplication.java
@@ -0,0 +1,13 @@
+package kr.ssok.ssom.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsomBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsomBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/ssok/ssom/backend/SsomBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ssok.ssom.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsomBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

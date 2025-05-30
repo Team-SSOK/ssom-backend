8ab5caf09072595a6270a8cfe285a68d7181e5d9
hanzyn09 <ysurkkr12@naver.com>
2025-05-30 14:44:36 +0900

    fix: argoCD 및 Jenkins 알림 제외 (#22, SCRUM-207)

--- src/main/java/kr/ssok/ssom/backend/domain/alert/service/AlertService.java
+++ src/main/java/kr/ssok/ssom/backend/domain/alert/service/AlertService.java
@@ -1,9 +1,7 @@
 package kr.ssok.ssom.backend.domain.alert.service;
 
 import jakarta.servlet.http.HttpServletResponse;
-import kr.ssok.ssom.backend.domain.alert.dto.AlertModifyRequestDto;
-import kr.ssok.ssom.backend.domain.alert.dto.AlertRequestDto;
-import kr.ssok.ssom.backend.domain.alert.dto.AlertResponseDto;
+import kr.ssok.ssom.backend.domain.alert.dto.*;
 import kr.ssok.ssom.backend.domain.alert.entity.constant.AlertKind;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
@@ -11,10 +9,16 @@ import java.util.List;
 
 public interface AlertService {
     SseEmitter subscribe(String username, String lastEventId, HttpServletResponse response);
-    List<AlertResponseDto> createAlert(AlertRequestDto request, AlertKind kind);
-    //void sendAlertToUsers(AlertSendRequestDto request);
-    //List<AlertResponseDto> getAlertsByKind(Long userId, AlertKind kind);
     List<AlertResponseDto> getAllAlertsForUser(String employeeId);
     void modifyAlertStatus(AlertModifyRequestDto request);
+
+    List<AlertResponseDto> createAlert(AlertRequestDto request, AlertKind kind);
+    List<AlertResponseDto> createGrafanaAlert(AlertGrafanaRequestDto alertGrafanaRequestDto);
+    List<AlertResponseDto> createOpensearchAlert(AlertOpensearchRequestDto alertOpensearchRequest);
+    List<AlertResponseDto> createIssueAlert(AlertIssueRequestDto alertIssueRequest);
+    //List<AlertResponseDto> createDevopsAlert(AlertSendRequestDto alertSendRequest);
+
+    //void sendAlertToUsers(AlertSendRequestDto request);
+    //List<AlertResponseDto> getAlertsByKind(Long userId, AlertKind kind);
 }
 

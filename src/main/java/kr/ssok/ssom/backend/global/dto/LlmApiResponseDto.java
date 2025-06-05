5475c6cbb593f9a80f12bd6fdc1c9f079efbbd50
hyun minyoung <kingmandoo95@gmail.com>
2025-06-05 10:46:23 +0900

    feat : llm issue 초안 작성 response dto boolean -> Boolean

--- src/main/java/kr/ssok/ssom/backend/global/dto/LlmApiResponseDto.java
+++ src/main/java/kr/ssok/ssom/backend/global/dto/LlmApiResponseDto.java
@@ -12,7 +12,7 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 public class LlmApiResponseDto<T> {
-    private boolean isSuccess;
+    private Boolean isSuccess;
     private String code;
     private String message;
     private List<T> result;
